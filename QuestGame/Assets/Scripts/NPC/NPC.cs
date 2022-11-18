78f8ca404671148895a9994d9f2fd698fc3ec76f
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2022-11-18 14:49:51 +0500

    Сделал диалоги

--- QuestGame/Assets/Scripts/NPC/NPC.cs
+++ QuestGame/Assets/Scripts/NPC/NPC.cs
@@ -1,16 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class NPC
+public abstract class NPC : MonoBehaviour, IInteractable
 {
+    private DialogueManager dialogueManager;
+    private DialoguePlayer dialoguePlayer;
     public abstract int NPCId { get; }
     public int DialogueId { get => CheckId(); }
 
     public abstract int CheckId();
 
+    public void Interact()
+    {
+        PlayDialogue();
+    }
+
     public void PlayDialogue()
     {
+        var phrases = dialogueManager.GetDialogue(this);
+        dialoguePlayer.PlayDialogue(phrases);
+    }
 
+    private void Awake()
+    {
+        var dialogueController = GameObject.Find("DialogueController");
+        dialogueManager = dialogueController.GetComponent<DialogueManager>();
+        dialoguePlayer = dialogueController.GetComponent<DialoguePlayer>();
     }
 }
