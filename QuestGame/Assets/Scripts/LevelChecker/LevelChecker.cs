1f87f53f7641822184385bf4bbe951a889b07dd2
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2022-12-05 18:29:55 +0500

    Реализовал LevelChecker
    Этот объект хранит информацию, какой квест пройден, какой нет

---
+++ QuestGame/Assets/Scripts/LevelChecker/LevelChecker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelChecker 
+{
+    public static bool IsTakeBag { get; set; }
+    public static bool IsTakeMap { get; set; }
+    public static bool IsSolveQuestLever { get; set; }
+    public static bool IsSolveQuestInCastleWithSearchItems { get; set; }
+    public static bool IsHeroTalkAIFirstTime { get; set; }
+    public static bool IsHeroTakeTaskBartender { get; set; }
+    public static bool IsHeroTakeTaskBlacksmith { get; set; }
+    public static bool IsHeroTakeTaskMinerWithGears { get; set; }
+    public static bool IsHeroDidTaskMinerWithGears { get; set; }
+    public static bool IsHeroTakePickaxeAndHelpMinerWithMechanism { get; set; }
+    public static bool IsHeroExtractedOre { get; set; }
+    public static bool IsHeroHelpBlacksmithWithOven { get; set; }
+    public static bool IsHeroReceivedHintFromBartender { get; set; }
+    public static bool IsHeroFindFirstPartAI { get; set; }
+    public static bool IsHeroReceivedHintFromMiner { get; set; }
+    public static bool IsHeroFindSecondPartAI { get; set; }
+    public static bool IsHeroRepairBridge { get; set; }
+    public static bool IsHeroFindThirdPartAI { get; set; }
+    public static bool IsHeroActivatedPortal { get; set; }
+}
