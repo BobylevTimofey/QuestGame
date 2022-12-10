56305d4ed2e2c3c5fb7b374dc3fb7a70185a1a8d
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2022-12-10 21:48:11 +0500

    Сделал mvp железного месторождения

---
+++ QuestGame/Assets/FolderForTest/TestIronOre/IronDeposit.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IronDeposit : MonoBehaviour
+{
+    public GameObject IronOre;
+    private int counter = 0;
+    public Slider MiningProgress;
+    private readonly int WaitSeconds = 1;
+    private int maxCount = 10;
+    private int currentCount = 0;
+
+    private void OnMouseDown()
+    {
+        if (currentCount < maxCount)
+        {
+            counter++;
+            MiningProgress.gameObject.SetActive(true);
+            MiningProgress.value = counter;
+            if (counter == 3)
+            {
+                SpawnIronOre();
+                counter = 0;
+                currentCount++;
+            }
+        }
+    }
+    private void OnMouseUp()
+    {
+        StartCoroutine(WaitAndCloseProgress(WaitSeconds));
+    }
+
+    IEnumerator WaitAndCloseProgress(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        MiningProgress.gameObject.SetActive(false);
+    }
+
+    private void SpawnIronOre()
+    {
+        Instantiate(IronOre, transform.position + new Vector3(0, 1), Quaternion.identity);
+    }
+}
